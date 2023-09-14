465d3255cb2e4f90eb258500e999ed8435e60f66
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2023-09-14 10:59:24 +0200

    afsadasfdfdsa

--- quizeee/Program.cs
+++ quizeee/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] hehe ={"bob", "bib", "bo"};
+
+System.Console.WriteLine(hehe[2]);
+
+string[][] booo= {
+ new string[]  {"frugan", "bra"},
+ new string[]  {},
+ new string[]  {}
+};
+void beb(string fro, string ret){
+    System.Console.WriteLine(booo);
+    string sva = Console.ReadLine();
+
+    if(sva == ret){
+        System.Console.WriteLine("bra");
+    }
+}
+System.Console.WriteLine(booo);
+Console.ReadLine();
